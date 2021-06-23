6a14ad1ae1c1dd9eb971c7cd5ff9fb2488e1ce47
AmosChenYQ <amoschenyq@foxmail.com>
2021-06-23 21:06:12 +0800

    finish movie detail page except casts info cards

--- pages/movie-detail/movie-detail.js
+++ pages/movie-detail/movie-detail.js
@@ -1,18 +1,51 @@
 // pages/movie-detail/movie-detail.js
+const app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    movie: {}
+  },
 
+  onViewMovieImage(event) {
+    console.log(event);
+    wx.previewImage({
+      urls: [this.data.movie.images.large],
+    })
+  },
+
+  prepProcessMovieData(movie) {
+    movie.subTitle = `${movie.countries.toString()} · ${movie.year}`;
+    movie.castsNames = movie.casts.map(item => item.name).join(" / ");
+    movie.castsInfo = movie.casts.map((item) => {
+      return {
+        img: item?.avatars?.large || "",
+        name: item.name
+      }
+    });
+    movie.directorsNames = movie.directors.map(item => item.name).join("/");
+    movie.genres = movie.genres.join(', ');
+    console.log(movie);
+    this.setData({movie});
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(options);
+    wx.request({
+      url: `${app.gBaseUrl}/subject/${options.movieId}`,
+      success: (res) => {
+        // console.log(res.data);
+        this.prepProcessMovieData(res.data);
+        // res.data.subTitle = `${res.data.countries.toString()} · ${res.data.year}`
+        // this.setData({movie: res.data});
+      }
+    });
   },
 
   /**
